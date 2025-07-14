adb5f5040c0835948cf844827a19d72de3bd3274
Nik Revenco <pm@nikrev.com>
2025-07-14 20:08:28 +0100

    chore: remove unused dependency

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -9,42 +8,60 @@
   };
 
   outputs =
+    { nixpkgs, rust-overlay, ... }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forEachSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          let
+            overlays = [ (import rust-overlay) ];
+            pkgs = import nixpkgs {
+              inherit system overlays;
+            };
+            nativeBuildInputs = [
+              (pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+            ];
+          in
+          f {
+            inherit system pkgs nativeBuildInputs;
+          }
+        );
+    in
     {
-      nixpkgs,
-      flake-utils,
-      rust-overlay,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-        nativeBuildInputs = [
-          (pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
-        ];
-        manifest = pkgs.lib.importTOML ./Cargo.toml;
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          inherit nativeBuildInputs;
-          # This is needed or rust-analyzer will not work correctly.
-          # Source: https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570
-          RUST_SRC_PATH = "${
-            pkgs.rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; }
-          }/lib/rustlib/src/rust/library";
-        };
-        packages.default = pkgs.rustPlatform.buildRustPackage {
-          pname = manifest.package.name;
-          version = manifest.package.version;
+      devShells = forEachSystem (
+        { pkgs, nativeBuildInputs, ... }:
+        {
+          default = pkgs.mkShell {
+            inherit nativeBuildInputs;
+            buildInputs = [ pkgs.nixfmt-rfc-style ];
+          };
+        }
+      );
 
-          src = pkgs.lib.cleanSource ./.;
-          cargoLock.lockFile = ./Cargo.lock;
+      packages = forEachSystem (
+        { pkgs, nativeBuildInputs, ... }:
+        {
+          default =
+            let
+              manifest = pkgs.lib.importTOML ./Cargo.toml;
+            in
+            pkgs.rustPlatform.buildRustPackage {
+              pname = manifest.package.name;
+              version = manifest.package.version;
+              src = pkgs.lib.cleanSource ./.;
+              cargoLock.lockFile = ./Cargo.lock;
 
-          inherit nativeBuildInputs;
-        };
-      }
-    );
+              inherit nativeBuildInputs;
+            };
+        }
+      );
+    };
 }
